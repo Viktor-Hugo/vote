a5be344e32b37f1c93fe4d2b3b84ac0249e954f2
김구현 <viktor@forestsoft.co.kr>
2024-05-14 13:01:27 +0900

    origin

--- back/bid_management/settings.py
+++ back/bid_management/settings.py
@@ -32,10 +32,10 @@ ALLOWED_HOSTS = [
     'https://vote-tqrw.onrender.com/'
 ]
 
-# CORS_ALLOWED_ORIGINS = [
-#     'http://127.0.0.1:5173',
-#     'http://localhost:5173',
-# ]
+CORS_ALLOWED_ORIGINS = [
+    'https://vote-1.onrender.com',
+    # 'http://localhost:5173',
+]
 
 # Application definition
 
